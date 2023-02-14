dc58f6c81d8ae7082c89ae9b9dd804cff5eb9a9d
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-02-14 17:58:36 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 116 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 출력문을 먼저 작성한 후 " " 안에 붙여넣기 하면됨.
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
