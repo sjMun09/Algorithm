870160c8112942527bfe7baab5b97f37eda60667
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-02-14 13:55:09 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 148 ms, Memory: 15896 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -6,24 +6,24 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args)  {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int[] arr = new int[26];
 
         for (int i = 0; i < arr.length; i++) {
-            arr[i]= -1;
+            arr[i]=-1;
         }
-        String s = sc.nextLine(); //엔터 치기 전까지 받음
+        String s = br.readLine(); // 문자열 스트림으로 받음
 
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-
-            if (arr[ch - 'a'] == -1) {
-                arr[ch - 'a'] =i;
+            if (arr[ch - 'a'] == -1) { // 원소길이 -1일 경우만 초기화
+                arr[ch - 'a'] = i;
             }
         }
-        for (int var : arr) {
+        for (int var : arr) { // 배열 출력
             System.out.print(var + " ");
         }
     }
