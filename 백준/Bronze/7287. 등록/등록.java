f14d47fbdc5f2070a4d5c37c0ceab3eb44679e98
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-02-14 14:29:31 +0900

    [Bronze V] Title: 등록, Time: 116 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args)  {
+
+        System.out.println(32);
+        System.out.println("ohoh7391");
+    }
+}
