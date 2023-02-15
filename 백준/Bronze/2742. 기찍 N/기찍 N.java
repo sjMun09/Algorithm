1a1621d6b7b00d2eb783d6b9dcac29d39331786d
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-02-15 12:46:17 +0900

    [Bronze IV] Title: 기찍 N, Time: 1000 ms, Memory: 29972 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = N; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+    }
+}
