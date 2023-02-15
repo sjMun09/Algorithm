718942373ad372efd5b73a44abc3bbfc21b6678a
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-02-15 12:48:21 +0900

    [Bronze IV] Title: 기찍 N, Time: 964 ms, Memory: 29716 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.java
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -7,9 +7,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-
-        for (int i = N; i >= 1; i--) {
-            System.out.println(i);
+        
+        //for 반복문 사용하지 않고 코드길이 최대한 간결 + 빠른 출력을 하는 logic
+        while( N > 0 ) {
+            System.out.println(N);
+            N--;
         }
 
     }
