04a5928541dc86fbc41448fe0f41c65a8b5355c7
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-02-15 13:17:00 +0900

    [Bronze V] Title: We love kriii, Time: 112 ms, Memory: 13812 KB -BaekjoonHub

--- 백준/Bronze/10718. We love kriii/We love kriii.java
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[]args){
-        System.out.println("강한친구 대한육군");
-        System.out.println("강한친구 대한육군");
+        System.out.print("강한친구 대한육군\n");
+        System.out.print("강한친구 대한육군\n");
     }
 }
