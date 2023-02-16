6c598a618c6ea283b13a77a7d96a1ef7a8b28643
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-02-16 11:19:45 +0900

    [Bronze IV] Title: 세수정렬, Time: 140 ms, Memory: 15816 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int[] arr = new int[3];
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        //배열정렬
+        Arrays.sort(arr);
+        for (int i = 0; i < 3; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
