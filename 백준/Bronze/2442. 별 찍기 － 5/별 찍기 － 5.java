6f1714889a917ee20f8c6a972d437ef83ed24c75
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-02-20 14:45:12 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 304 ms, Memory: 16600 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            for (int j =  1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2*i-1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
