1a344ef127c686de5f77db3a307806b297ce28af
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-02-22 13:57:20 +0900

    [Bronze III] Title: 전자레인지, Time: 160 ms, Memory: 16320 KB -BaekjoonHub

--- 백준/Bronze/10162. 전자레인지/전자레인지.java
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -6,15 +6,16 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int T = Integer.parseInt(br.readLine());
         int A,B,C;
-        if((T % 300 % 60 % 10) == 0){
+        if ((T % 300 % 60 % 10) !=0) {
+            bw.write("-1");
+        }else {
             A = T / 300;
             B = (T % 300) / 60;
             C = ((T % 300) % 60) / 10;
-            bw.write(A +" "+ B +" "+ C);
-        }else{
-            bw.write("-1");
+            bw.write(A + " " + B + " " + C);
         }
         bw.flush();
         bw.close();
