0ed6ee00a93c0e462cfe382bda6dbd25258b9eb4
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-02-23 20:18:06 +0900

    [Bronze III] Title: 소음, Time: 120 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/2935. 소음/소음.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        BigInteger a = new BigInteger(br.readLine());
+        String sub = br.readLine();
+        BigInteger b = new BigInteger(br.readLine());
+        if (sub.equals("*")) {
+            System.out.print(a.multiply(b));
+        } else {
+            System.out.print(a.add(b));
+        }
+    }
+}
