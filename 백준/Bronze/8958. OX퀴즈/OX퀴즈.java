8d58974d8a857428410994f563bf7cb2d7cbcee0
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-03-05 14:35:11 +0900

    [Bronze II] Title: OX퀴즈, Time: 124 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -8,29 +8,23 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int test_case = Integer.parseInt(br.readLine()); // 테스트 케이스
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
 
-        for (int i = 0; i < test_case; i++) {
-
-            int count = 0; // 연속횟수
-            int sum = 0; // 누적 합산
+            int count = 0;
+            int sum = 0;
 
             for (byte value : br.readLine().getBytes()) {
-
                 if (value == 'O') {
                     count++;
                     sum += count;
-                }
-                else {
+                } else {
                     count = 0;
                 }
             }
 
             sb.append(sum).append('\n');
-
         }
         System.out.println(sb);
-
-
     }
 }
