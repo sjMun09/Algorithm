0105e4b2657a3f1f3192303f8791ac1eb4fc62aa
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-03-07 11:56:50 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,21 +1,21 @@
 import java.io.*;
 import java.math.BigInteger;
 import java.util.*;
+
 public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-        
-		int A = in.nextInt();
-		int B = in.nextInt();
-        
-        in.close();
-        
-		A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
-		B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
-		
-		System.out.print(A > B ? A : B);
-	
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+//      토큰을 생성해서 띄어쓰기로 각 토큰을 나눠줌
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+//      나눠준 토큰을 StringBuilder의 reverse() 메소드를 사용해서 문자열을 뒤집어줌. 
+//      그리고 StringBuilder 타입을 문자열로 반환시키기 위해 toString()메소드 사용. 이후 Integer.parseInt()로 문자열을 다시 숫자로 바꿔줌
+        int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        System.out.print(A > B ? A:B);
+
+    }
 }
+
