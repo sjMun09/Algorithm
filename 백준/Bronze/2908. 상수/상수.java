071f7ca6f13f82b04469ca633216fd2dc31a515b
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-03-07 11:46:16 +0900

    [Bronze II] Title: 상수, Time: 212 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+public class Main {
+ 
+	public static void main(String[] args) {
+ 
+		Scanner in = new Scanner(System.in);
+        
+		int A = in.nextInt();
+		int B = in.nextInt();
+        
+        in.close();
+        
+		A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+		B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+		
+		System.out.print(A > B ? A : B);
+	
+	}
+}
