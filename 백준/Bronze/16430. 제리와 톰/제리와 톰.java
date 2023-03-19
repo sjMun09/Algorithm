664279e3a69b28e9d7196b1901ca29ab95921aa7
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-03-19 20:44:16 +0900

    [Bronze V] Title: 제리와 톰, Time: 148 ms, Memory: 16196 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.print(B - A + " " + B);
+        
+    }
+}
+
