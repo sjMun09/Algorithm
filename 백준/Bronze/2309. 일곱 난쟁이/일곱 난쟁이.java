ddf91975579ed67f77b02f99b93ad961168cce26
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-04-06 22:07:00 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 160 ms, Memory: 16320 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,35 +1,55 @@
 import java.io.*;
-import java.math.BigInteger;
 import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
-    static int[] height = new int[9]; // 9개의 정수를 저장할 배열을 선언합니다.
+
+    static int N = 9;
+    static int [] height = new int[N];
+
+    // 일곱 난쟁이를 선택하는 데 사용할 예정
+    static boolean[] selected = new boolean[N];
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // BufferedReader를 사용하여 입력을 받습니다.
-
-        int sum = 0; // 일곱 난쟁이의 키의 합을 계산하기 위해 sum 변수를 0으로 초기화합니다.
-
-        for (int i = 0; i < 9; i++) { // 9개의 난쟁이 키를 입력받기 위해 for문을 사용합니다.
-            height[i] = Integer.parseInt(br.readLine()); // 입력받은 키를 배열에 저장합니다.
-            sum += height[i]; // 입력받은 키를 sum 변수에 더합니다.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 일곱 난쟁이의 키의 합이 100
+        for (int i = 0; i < N; i++) {
+            height[i] = Integer.parseInt(br.readLine());
         }
+        findSevenDwarf(0, 0, 0);
+    }
 
-        Arrays.sort(height); // 입력받은 키를 오름차순으로 정렬합니다.
 
-        for (int i = 0; i < 9; i++) { // 첫 번째 난쟁이부터 아홉 번째 난쟁이까지 순서대로 탐색합니다.
-            for (int j = i + 1; j < 9; j++) { // i 번째 난쟁이와 j 번째 난쟁이를 선택하여 키의 합을 구합니다.
-                if (sum - height[i] - height[j] == 100) { // 합이 100인 경우
-                    for (int k = 0; k < 9; k++) { // 9명 중에서 2명을 제외한 7명의 키를 출력합니다.
-                        if (k != i && k != j) {
-                            System.out.println(height[k]);
-                        }
-                    }
-                    System.exit(0); // 프로그램을 종료합니다.
+    // cnt 는 선택된 난쟁이의 수,  는 선택할 수 있는 난쟁이 중에서 현재 선택한 난쟁이의 다음 난쟁이를 가리킴
+    static void findSevenDwarf(int cnt, int sum, int start) {
+        // 7명이고 키 합이 100일때
+        if (cnt == 7 && sum == 100) {
+            printSevenDwarf();
+            System.exit(0); // 강제종료
+        }
+        if (cnt < 7) {
+            for (int i = start; i < N; i++) {
+                if (!selected[i]) {
+                    selected[i] = true;
+                    findSevenDwarf(cnt + 1, sum + height[i], i+1);
+                    selected[i] = false;
                 }
             }
+        }
+
+    }
+
+    static void printSevenDwarf(){
+        // dwarf == 난쟁이
+        int[] dwarf = new int[7];
+        int index =0;
+        for (int i = 0; i < N; i++) {
+            if (selected[i]){
+                dwarf[index++] = height[i];
+            }
+        }
+        Arrays.sort(dwarf);
+        for (int i = 0; i < dwarf.length; i++) {
+            System.out.println(dwarf[i]);
         }
     }
 }
