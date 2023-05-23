f92596897c3344981451511b30aae4e6807371c8
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-05-23 17:12:03 +0900

    [Silver IV] Title: 보물, Time: 124 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -5,35 +5,38 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
 
-        int arrayLength = Integer.parseInt(br.readLine());
-        int[] arrayA = new int[arrayLength];
+        int n = Integer.parseInt(br.readLine());
+        int[] arrayA = new int[n];
+        int [] arrayB = new int [n];
+        int sum = 0;
+        
+        // A 배열에 대한 입력 처리
         st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < arrayLength; i++) {
+        for (int i = 0; i < n; i++) {
             arrayA[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arrayA); // 오름차순 소팅
-
-        Integer[] arrayB = new Integer[arrayLength];
+        
+        // B 배열에 대한 입력 처리
         st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < arrayLength; i++) {
+        for (int i = 0; i < n; i++) {
             arrayB[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arrayB, Comparator.reverseOrder()); // 내림차순 소팅 
-
-        int result = 0;
-        for (int i = 0; i < arrayLength; i++) {
-            result += arrayA[i] * arrayB[i];
+        
+        Arrays.sort(arrayA); // 오름차순 소팅
+        Arrays.sort(arrayB); // 오름차순 소팅
+        int[] reverseB = new int[n]; // 내림차순으로 정렬할 새 배열 생성
+        
+        // B 배열을 내림차순으로 정렬
+        for(int i = 0; i < n; i++) {
+            reverseB[i] = arrayB[n - i - 1];
         }
 
-        wr.write(result + "\n");
-        wr.flush();
-        wr.close();
-        br.close();
+        for (int i = 0; i < n; i++) {
+            sum += arrayA[i] * reverseB[i];
+        }
+        System.out.print(sum);
     }
 
 }
