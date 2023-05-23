2d7767e946d8152ff55e628a0a6aa8574add0c34
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-05-23 19:26:31 +0900

    [Silver IV] Title: 보물, Time: 124 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -1,42 +1,40 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-
-    public static void main(String[] args) throws Exception {
+public class Main{
+    public static void main(String []args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
         StringTokenizer st;
-
+        
         int n = Integer.parseInt(br.readLine());
-        int[] arrayA = new int[n];
+        int [] arrayA = new int [n];
         int [] arrayB = new int [n];
         int sum = 0;
         
-        // A 배열에 대한 입력 처리
+        // a
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
             arrayA[i] = Integer.parseInt(st.nextToken());
         }
-        
-        // B 배열에 대한 입력 처리
+        // b
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
             arrayB[i] = Integer.parseInt(st.nextToken());
         }
+        // ST
+        Arrays.sort(arrayA); // 오름차순 
+        Arrays.sort(arrayB); // 오름차순 정렬
+        int [] reverseB = new int [n]; // reverse 
         
-        Arrays.sort(arrayA); // 오름차순 소팅
-        Arrays.sort(arrayB); // 오름차순 소팅
-        int[] reverseB = new int[n]; // 내림차순으로 정렬할 새 배열 생성
-        
-        // B 배열을 내림차순으로 정렬
-        for(int i = 0; i < n; i++) {
-            reverseB[i] = arrayB[n - i - 1];
+        // b 정렬
+        for(int i=0; i<n;i++){
+            reverseB[i] =arrayB[n-i-1];
         }
-
-        for (int i = 0; i < n; i++) {
-            sum += arrayA[i] * reverseB[i];
+        for(int j =0; j<n; j++){
+            sum+= arrayA[j] * reverseB[j];
         }
         System.out.print(sum);
+        
     }
-
 }
