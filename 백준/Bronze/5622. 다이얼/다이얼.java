9bf4be61e70f945f74c68b86f2f4f9dca46e1a59
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-05-24 19:22:02 +0900

    [Bronze II] Title: 다이얼, Time: 120 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,54 +1,20 @@
 import java.io.*;
-import java.math.BigInteger;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//  문자열로 받고 문자열로 받은 것을 문자로 하나씩 인식해서 그 인식 번호가 w는 9번 -> 스위치문 써서 더해줌
+        String input = br.readLine().toUpperCase();
+        String[] dial = {"ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"};
+        int totalSeconds = 0;
 
-        String s = br.readLine();
-
-        int count =0;
-        int t = s.length();
-
-        for (int i = 0; i < t; i++) {
-            switch (s.charAt(i)) {
-                case 'A' : case 'B': case 'C' :
-                    count += 3;
-                    break;
-
-                case 'D' : case 'E': case 'F' :
-                    count += 4;
-                    break;
-
-                case 'G' : case 'H': case 'I' :
-                    count += 5;
-                    break;
-
-                case 'J' : case 'K': case 'L' :
-                    count += 6;
-                    break;
-
-                case 'M' : case 'N': case 'O' :
-                    count += 7;
-                    break;
-
-                case 'P' : case 'Q': case 'R' : case 'S' :
-                    count += 8;
-                    break;
-
-                case 'T' : case 'U': case 'V' :
-                    count += 9;
-                    break;
-
-                case 'W' : case 'X': case 'Y' : case 'Z' :
-                    count += 10;
-                    break;
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            for (int j = 0; j < dial.length; j++) {
+                if (dial[j].contains(Character.toString(ch))) {
+                    totalSeconds += (j+3);
+                }
             }
         }
-        System.out.println(count);
-
+        System.out.print(totalSeconds);
     }
 }
-
