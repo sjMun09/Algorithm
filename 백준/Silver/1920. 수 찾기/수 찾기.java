fe9f0b21efc0391fee47932e0533aca8582e1de9
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-05-26 12:38:00 +0900

    [Silver IV] Title: 수 찾기, Time: 1328 ms, Memory: 51408 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.io.*;
 
+// binarySearch 함수 안쓰고 구현한 코드
 public class Main{
     public static void main(String[]args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -22,11 +23,36 @@ public class Main{
             arr2[j]= Integer.parseInt(st.nextToken());
         }
         
-        for(int k=0; k<arr2.length;k++){
-            int idx = Arrays.binarySearch(arr1, arr2[k]);
-            if( idx>=0)System.out.print("1\n");
-            else System.out.print("0\n");
+        for (int k = 0; k < arr2.length; k++) {
+            if (binarySearch(arr1, arr2[k])) {
+                System.out.print("1\n");
+            } else {
+                System.out.print("0\n");
+            }
         }
-        
+        /*
+         이진 탐색 알고리즘을 사용하여 제공된 배열 (여기서는 arr1)에서
+         주어진 목표 값 (arr2[k])을 찾습니다.
+         binarySearch() 함수는 배열과 목표 값을 인수로 받습니다. 
+         배열에서 목표 값을 찾으면 true를 반환하고, 목표 값이 없으면 false를 반환
+        */
+    }
+    public static boolean binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if(arr[mid] == target) {
+                return true;
+            } else if (arr[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
     }
 }
+
