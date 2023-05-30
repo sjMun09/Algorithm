450a7ad7a6f5f5684726c56fca9379af7a70a94c
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-05-30 19:29:09 +0900

    [Bronze II] Title: 벌집, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -5,26 +5,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        // 로직,
-        // 1일때 벌집 갯수 1 , 2일때 6 , 3일때 12, 4일때 18, 5일때 (61-38+1)=24
-        // 방의 개수는 6의배수 임을 알 수 있다.
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         int N = Integer.parseInt(br.readLine());
+        int count = 1;
+        int range = 1;
+        int temp = 1;
 
-        // 둘다 최소값 기준
-        int honeyComb = 1;
-        int neighbor = 2;
+        while (temp < N) {
+            temp = temp + (count * 6);
+            range++;
+            count++;
+        }
 
-        if (N == 1) {
-            System.out.println(1);
-        } else {
-//          N보다 커지기 전까지만 반복해야됨
-            while (neighbor <= N) {
-                neighbor = neighbor + (6 * honeyComb);
-                honeyComb++;
-            }
-            System.out.println(honeyComb);
-        } 
+        bw.write(Integer.toString(range));
+        bw.newLine(); // 결과값 출력이후, 줄바꿈
+        bw.flush();
+        bw.close();
+        br.close();
         
     }
 }
