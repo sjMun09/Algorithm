f342906bf0c7115667f24013ce586ea2f3fad243
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-06-01 19:50:39 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 76 ms, Memory: 11596 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.java
@@ -1,35 +1,31 @@
+import java.util.*;
 import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[]args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        String[] fileNames = new String[n];
-      
-        for (int i = 0; i < n; i++) {
-            fileNames[i] = br.readLine();
+        StringBuilder sb= new StringBuilder();
+        
+        String[] FN = new String[n]; // input
+        
+        for(int i=0;i<n;i++){
+            FN[i]=br.readLine();
         }
-
-        StringBuilder answer = new StringBuilder();
-      
-        for (int j = 0; j < fileNames[0].length(); j++) {
-            char ch = fileNames[0].charAt(j);
-            boolean isSame = true;
-
-            for (int i = 1; i < n; i++) {
-                if (ch != fileNames[i].charAt(j)) {
+        
+        for(int j=0;j<FN[0].length();j++){
+            char ch = FN[0].charAt(j);
+            boolean isSame =true;
+            
+            for(int k=1;k<n;k++){
+                if(ch!=FN[k].charAt(j)){
                     isSame = false;
                     break;
                 }
-            }
-
-            if (isSame) {
-                answer.append(ch);
-            } else {
-                answer.append('?');
-            }
+            } 
+            if(isSame) sb.append(ch);
+                else sb.append("?");
         }
-      
-        System.out.println(answer);
+        System.out.print(sb);
     }
 }
