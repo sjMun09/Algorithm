2ef4640fa525631602648c0f24b4b3a3d71b8b34
SeongJunMun <124541156+sjMun09@users.noreply.github.com>
2023-06-08 20:40:11 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 80 ms, Memory: 11528 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,55 +1,55 @@
+// 선택한 난쟁이들 수를 7이, 합이 100인 조합을 찾을거에요.
+import java.util.*;
 import java.io.*;
-import java.util.Arrays;
 
-public class Main {
+class Main{
 
-    static int N = 9;
-    static int [] height = new int[N];
+    static int N = 9; // 9명
+    static int[] height = new int[9]; // 9명 input 그대로 초기화
+    static int a = 0;
+    static boolean[] selected = new boolean[N]; // 초이스할 때, boolean을 사용
 
-    // 일곱 난쟁이를 선택하는 데 사용할 예정
-    static boolean[] selected = new boolean[N];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 일곱 난쟁이의 키의 합이 100
-        for (int i = 0; i < N; i++) {
-            height[i] = Integer.parseInt(br.readLine());
+    public static void main(String[]args) throws IOException{
+        BufferedReader br  =new BufferedReader(new InputStreamReader(System.in));
+        for(int i=0; i<N;i++ ){
+            height[i]= Integer.parseInt(br.readLine());
         }
-        findSevenDwarf(0, 0, 0);
+        find(0,0,0);
+
     }
+    // count = 난쟁이 수, sum = 합계 100, satrt= 카운팅해줄거
+    static void find(int count, int sum, int start){
+        if(count == 7 && sum == 100){
+            print();
+            return;
 
-
-    // cnt 는 선택된 난쟁이의 수,  는 선택할 수 있는 난쟁이 중에서 현재 선택한 난쟁이의 다음 난쟁이를 가리킴
-    static void findSevenDwarf(int cnt, int sum, int start) {
-        // 7명이고 키 합이 100일때
-        if (cnt == 7 && sum == 100) {
-            printSevenDwarf();
-            System.exit(0); // 강제종료
         }
-        if (cnt < 7) {
-            for (int i = start; i < N; i++) {
-                if (!selected[i]) {
-                    selected[i] = true;
-                    findSevenDwarf(cnt + 1, sum + height[i], i+1);
+        /*
+        재귀 방식인데,
+         */
+        if(count <7){
+            for(int i=start;i<N;i++){  //  ~ 9
+                if(!selected[i]) selected[i]= true;
+                    find(count+1, sum+height[i], i+1);
                     selected[i] = false;
-                }
             }
         }
+    }
+
+    static void print(){
+        a++;
+
+        int[] dwarf = new int[7]; // 7명이어야 하니까
+        int index= 0;
+        for(int i=0;i<N;i++){
+            if(selected[i]) dwarf[index++] = height [i]; // 인덱스 민다고 생각/
+        }
+        Arrays.sort(dwarf); // sort
+        for(int j=0;j<7;j++)
+            if(a==1)
+            System.out.print(dwarf[j]+"\n");
 
     }
 
-    static void printSevenDwarf(){
-        // dwarf == 난쟁이
-        int[] dwarf = new int[7];
-        int index =0;
-        for (int i = 0; i < N; i++) {
-            if (selected[i]){
-                dwarf[index++] = height[i];
-            }
-        }
-        Arrays.sort(dwarf);
-        for (int i = 0; i < dwarf.length; i++) {
-            System.out.println(dwarf[i]);
-        }
-    }
 }
