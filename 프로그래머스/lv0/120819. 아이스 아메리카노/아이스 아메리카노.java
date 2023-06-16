bb57dd56ae6460b9a59148ac2845c4e6488281b6
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-06-16 16:41:04 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,9 +1,5 @@
 class Solution {
     public int[] solution(int money) {
-        int AA = 5500;
-        int calc = money/5500;
-        int balance = money%5500;
-        int[] answer = {calc, balance};
-        return answer;
+        return new int[] { money / 5500, money % 5500 };
     }
 }
