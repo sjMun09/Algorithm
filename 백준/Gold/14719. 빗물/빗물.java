c525770d60623ff7641988ef344c213a07450c77
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-06-18 20:07:38 +0900

    [Gold V] Title: 빗물, Time: 84 ms, Memory: 11836 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -7,31 +7,27 @@ public class Main {
         String[] input = br.readLine().split(" ");
         int H = Integer.parseInt(input[0]);
         int W = Integer.parseInt(input[1]);
-
         int[] blocks = new int[W];
         input = br.readLine().split(" ");
-
         for (int i = 0; i < W; i++) {
             blocks[i] = Integer.parseInt(input[i]);
         }
         int totalRainWater = 0;
-
-        // 블록 사이에 쌓인 빗물의 양
-        for (int i = 1; i < W - 1; i++) {
-            int leftMax = 0, rightMax = 0;
-            // 왼쪽 블록 중에서 가장 높은 블록찾음
-            for (int j = 0; j < i; j++) {
-                leftMax = Math.max(leftMax, blocks[j]);
-            }
-            // 오른쪽 블록 중에서 가장 높은 블록
-            for (int j = i + 1; j < W; j++) {
-                rightMax = Math.max(rightMax, blocks[j]);
-            }
-            // 현재 블록의 양쪽에서 가장 높은 블록 중 더 낮은 블록과 현재 블록 높이의 차이를 구해서 총량에 더하기
-            int minHeight = Math.min(leftMax, rightMax);
-            if (minHeight > blocks[i]) {
-                totalRainWater += minHeight - blocks[i];
-            }
+        int leftMax = 0;
+        int[] leftMaxs = new int[W];
+        for (int i = 0; i < W; i++) {
+            leftMax = Math.max(blocks[i], leftMax);
+            leftMaxs[i] = leftMax;
+        }
+        int rightMax = 0;
+        int[] rightMaxs = new int[W];
+        for (int i = W - 1; i >= 0; i--) {
+            rightMax = Math.max(blocks[i], rightMax);
+            rightMaxs[i] = rightMax;
+        }
+        for (int i = 0; i < W; i++) {
+            int minHeight = Math.min(leftMaxs[i], rightMaxs[i]);
+            totalRainWater += minHeight - blocks[i];
         }
         bw.write(Integer.toString(totalRainWater));
         bw.newLine();
