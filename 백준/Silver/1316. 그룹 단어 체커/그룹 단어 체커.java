a5e5e9aca3b4045eadf39203c627c6696ee1debf
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-06-18 19:18:14 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 76 ms, Memory: 11652 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,35 +1,33 @@
+import java.util.*;
 import java.io.*;
 
 class Main{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[]args) throws IOException{
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw  = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
         int count = 0;
-
-        for (int i = 0; i < N; i++) {
+        // input
+        for(int i =0;i<N;i++){
             String word = br.readLine();
-            if (checkingWord(word)) count++;
+            if(checkingWords(word)) count++;
         }
         bw.write(String.valueOf(count));
         bw.flush();
         bw.close();
         br.close();
     }
-
-    public static boolean checkingWord(String words) {
-        boolean[] alphabet = new boolean[26];
-        for (int i = 0; i < words.length(); i++) {
-            char okChar = words.charAt(i);
-            if (!alphabet[okChar - 'a']) {
-                alphabet[okChar - 'a'] = true;
-                while (i + 1 < words.length() && words.charAt(i) == words.charAt(i + 1)) {
+    public static boolean checkingWords( String word){
+        boolean[] alphabet = new boolean[26]; // EJOTY
+        for(int i =0; i<word.length();i++){
+            char currentChar = word.charAt(i);
+            if(!alphabet[currentChar-'a']){
+                alphabet[currentChar-'a']= true; // 표시
+                while(i+1<word.length()&&word.charAt(i)== word.charAt(i+1)){
                     i++;
                 }
-            }else
-                return false;
-        }
-        return true;
+            }else return false;
+        }return true;
     }
 }
