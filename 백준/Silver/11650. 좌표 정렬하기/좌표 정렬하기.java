b2616d08829f30d18b988780218c74653e6e006c
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-06-21 19:15:18 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 1940 ms, Memory: 84836 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -11,9 +11,18 @@ class Main{
             points[i][0] = Integer.parseInt(st.nextToken());
             points[i][1] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(points, Comparator
-                         .comparingInt((int[] point) -> point[0])
-                         .thenComparingInt(point -> point[1]));
+       // Arrays.sort(points, Comparator
+       //                .comparingInt((int[] point) -> point[0])
+       //                .thenComparingInt(point -> point[1]));
+        Arrays.sort(points,new Comparator<int[]>(){
+            public int compare(int[]point1, int[]point2){
+                if(point1[0]==point2[0]){
+                    return Integer.compare(point1[1], point2[1]);
+                }return Integer.compare(point1[0], point2[0]);
+            }
+        });
+        
+        
         for(int i=0;i<N;i++){
             System.out.println(points[i][0]+" "+points[i][1]);
         }
