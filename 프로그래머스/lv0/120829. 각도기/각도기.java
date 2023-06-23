ec83593fa5b25cd0a0b8919c21fedf61ba4b892e
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-06-23 14:52:05 +0900

    [level 0] Title: 각도기, Time: 0.02 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int angle) {
+        if(angle ==180){
+            return 4;
+        }else if(angle>90&&angle<180){
+            return 3;
+        }else if(angle==90){
+            return 2;
+        }else return 1;
+    }
+}
