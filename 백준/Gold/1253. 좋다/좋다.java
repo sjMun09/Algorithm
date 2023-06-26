d56211e9cb4dfe11aec854f43eda045de0fdb2dd
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-06-26 16:41:47 +0900

    [Gold IV] Title: 좋다, Time: 152 ms, Memory: 12828 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,40 +1,35 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        // 1253 번 좋다 문제는 투 포인터 문제임.
+        StringTokenizer st ;
         int N = Integer.parseInt(br.readLine());
-        int[] number = new int[N];
-
-        st = new StringTokenizer(br.readLine());
+        int[] A = new int[N]; 
+        st= new StringTokenizer(br.readLine());
+        // input
         for (int i = 0; i < N; i++) {
-            number[i] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
-
-        Arrays.sort(number);
-        int answer = 0;
+        // sort
+        Arrays.sort(A);
+        int count =0;
         for (int i = 0; i < N; i++) {
             int left = 0;
-            //  right 는 음수값이 존재함
-            int right = N - 1;
+            int right = N - 1; 
             while (true) {
-                // 현재위치 = i
-                if (left == i) left++;
+                if(left == i) left ++;
                 else if (right == i) right--;
-                // answer 존재 x
-                if (left >= right) break;
-                // 투 포인터로 밀어주는거임. 왼쪽으로 움직이는 right 값을 변경
-                if (number[left] + number[right] > number[i]) right--;
-                else if (number[left] + number[right] < number[i]) left++;
-                else {
-                    answer++;
+                if(left>= right) break;
+                if(A[left]+A[right] > A[i]) right--;
+                else if(A[left]+ A[right]< A[i]) left++;
+                else{
+                    count++;
                     break;
                 }
             }
         }
-        System.out.print(answer);
+        System.out.print(count);
     }
 }
