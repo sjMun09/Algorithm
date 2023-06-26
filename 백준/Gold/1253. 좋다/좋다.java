a74ec94a08524d0f4105fcbcffc0d9c062fec635
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-06-26 18:36:50 +0900

    [Gold IV] Title: 좋다, Time: 148 ms, Memory: 12648 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,35 +1,36 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main {
-    public static void main(String args[]) throws IOException {
+class Main{
+    public static void main(String[]args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st ;
-        int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N]; 
-        st= new StringTokenizer(br.readLine());
+        StringTokenizer st;
+        int N= Integer.parseInt(br.readLine());
+        int[] A = new int[N];
         // input
-        for (int i = 0; i < N; i++) {
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N;i++){
             A[i] = Integer.parseInt(st.nextToken());
         }
-        // sort
         Arrays.sort(A);
-        int count =0;
-        for (int i = 0; i < N; i++) {
-            int left = 0;
-            int right = N - 1; 
-            while (true) {
-                if(left == i) left ++;
-                else if (right == i) right--;
+        int answer =0;
+        for(int i=0; i<N;i++){
+            int left =0;
+            int right = N-1; // RIGHT index 
+            while(true){
+                //
+                if(left == i) left++;
+                else if(right == i) right--;
+                // answer 존재 x
                 if(left>= right) break;
-                if(A[left]+A[right] > A[i]) right--;
-                else if(A[left]+ A[right]< A[i]) left++;
+                if(A[left]+A[right]>A[i]) right--;
+                else if(A[left]+A[right]<A[i]) left++;
                 else{
-                    count++;
+                    answer++;
                     break;
                 }
             }
         }
-        System.out.print(count);
+        System.out.print(answer);
     }
 }
