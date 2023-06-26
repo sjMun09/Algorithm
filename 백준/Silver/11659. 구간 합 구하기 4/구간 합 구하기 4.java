5bf26ffd8271f9e8bd6beaf214152ccaa14ec2c5
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-06-26 16:02:41 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 2080 ms, Memory: 79648 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -7,14 +7,18 @@ public class Main {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int numCount = Integer.parseInt(st.nextToken());
-        int numCount2 = Integer.parseInt(st.nextToken());
-        long[] S = new long[numCount + 1];
+        int quizNum = Integer.parseInt(st.nextToken());
+        
+        long[] S = new long[numCount + 1]; // 질의항을 바로 사용하기 위해서 --> 인덱스는 원래 0번부터 시작
         st = new StringTokenizer(br.readLine());
+        
         for (int i = 1; i <= numCount; i++) {
-            S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
+            S[i] = S[i - 1] + Integer.parseInt(st.nextToken()); // 합배열 공식
         }
-        for (int q = 0; q < numCount2; q++) {
+        
+        for (int q = 0; q < quizNum; q++) {
             st = new StringTokenizer(br.readLine());
             int i = Integer.parseInt(st.nextToken());
             int j = Integer.parseInt(st.nextToken());
