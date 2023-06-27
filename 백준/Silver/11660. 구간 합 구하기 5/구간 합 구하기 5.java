ff358f814f841dc14f6a82ea288beb7dfc09d1ce
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-06-27 19:08:49 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 732 ms, Memory: 131452 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,34 +1,38 @@
 import java.util.*;
 import java.io.*;
-
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int A[][] = new int[N + 1][N + 1];
+        int[][] A = new int[N + 1][N + 1];
+
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
                 A[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int D[][] = new int[N + 1][N + 1];
+        int[][] D = new int[N + 1][N + 1];
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
+                // 구간 합 공식
                 D[i][j] = D[i][j - 1] + D[i - 1][j] - D[i - 1][j - 1] + A[i][j];
             }
         }
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int y1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
-            int answer = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
-            System.out.println(answer);
+            //
+            int result = D[x2][y2] - D[x1 - 1][y2] - D[x2][y1 - 1] + D[x1 - 1][y1 - 1];
+            sb.append(result).append('\n');
         }
-        
+        System.out.print(sb.toString());
     }
 }
