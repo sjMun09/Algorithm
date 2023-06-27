cebf8e7189d1f654af26c28760857209c72272db
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-06-27 13:06:40 +0900

    [Gold III] Title: 나머지 합, Time: 472 ms, Memory: 167896 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        long[] S = new long[N];
+        long[] C = new long[M];
+        long answer = 0;
+        st = new StringTokenizer(br.readLine());
+        S[0] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i < N; i++) {
+            S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < N; i++) {
+            int remainder = (int) (S[i] % M);
+            if(remainder==0) answer++;
+            C[remainder]++;
+        }
+        for (int i = 0; i < M;i++) {
+            if (C[i] > 1) {
+                answer = answer + (C[i] * (C[i] - 1) / 2);
+            }
+        }
+        System.out.print(answer);
+    }
+}
