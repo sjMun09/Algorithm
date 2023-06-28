d5be9b85763d1c21e8466de94632483b4dc0b4fe
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-06-28 22:51:59 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 44304 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> myQueue = new LinkedList<>();
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) myQueue.add(i);
+        while (myQueue.size() > 1) {
+            myQueue.poll();
+            myQueue.add(myQueue.poll());
+        }
+        System.out.print(myQueue.poll());
+    }
+}
