28cb600b4d49eb8eb2fcf4c8dd5df49cdbd36429
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-06-30 05:26:56 +0900

    [Platinum V] Title: 무서운 아르바이트, Time: 328 ms, Memory: 27500 KB -BaekjoonHub

--- 백준/Platinum/12846. 무서운 아르바이트/무서운 아르바이트.java
+++ 백준/Platinum/12846. 무서운 아르바이트/무서운 아르바이트.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    // 12846 플레티넘 5, 무서운 아르바이트,   stack 문제임.
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -19,19 +20,19 @@ public class Main {
         Stack<Integer> stack = new Stack<>();
         long maxMoney = 0;
 
-        for (int i = 0; i < pay.length; i++) {
-            while (!stack.isEmpty() && pay[stack.peek()] > pay[i]) {
-                int height = pay[stack.pop()];
-                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
-                maxMoney = Math.max(maxMoney, (long) height * width);
+        for (int i = 0; i <= pay.length; i++) {
+            // 스택이 비어있지 않고,
+            // i 가 배열의 총 길이와 같거나(stack에 있는 요소를 처리해야할 때)
+            // 또는 현재 스택의 맨 위 요소에 있는 인덱스의 값이 더 큰 경우
+            while (!stack.isEmpty() && (i == pay.length || pay[stack.peek()] > pay[i])) {
+                int poppedIndex = stack.pop(); // 스택에서 맨 위의 인덱스 제거
+                int height = pay[poppedIndex]; // 제거된 인덱스의 값(고려되는 타이어 세트 가격)
+                int leftIndex = stack.isEmpty() ? 0 : stack.peek() + 1; // 제거된 인덱스의 왼쪽 인덱스
+                int width = i - leftIndex; // 제거된 인덱스까지의 범위 길이
+                long calculatedMoney = (long) height * width; // 구한 범위와 가격을 곱하여 계산된 최대 가치
+                maxMoney = Math.max(maxMoney, calculatedMoney); // 최대 가치 업트
             }
-            stack.push(i);
-        }
-
-        while (!stack.isEmpty()) {
-            int height = pay[stack.pop()];
-            int width = stack.isEmpty() ? pay.length : pay.length - stack.peek() - 1;
-            maxMoney = Math.max(maxMoney, (long) height * width);
+            stack.push(i); // 현재 인덱스를 스택에 푸쉬
         }
 
         return maxMoney;
