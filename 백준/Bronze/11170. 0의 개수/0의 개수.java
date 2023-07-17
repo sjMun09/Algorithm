d1b548cc81595ee7a1bc58163d8c166e05d44273
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-07-17 18:08:41 +0900

    [Bronze I] Title: 0의 개수, Time: 728 ms, Memory: 293364 KB -BaekjoonHub

--- 백준/Bronze/11170. 0의 개수/0의 개수.java
+++ 백준/Bronze/11170. 0의 개수/0의 개수.java
@@ -1,27 +1,24 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
+class Main{
+    public static void main(String[]args )throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < t; i++) {
+        for(int i=0;i<t;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int n = Integer.parseInt(st.nextToken());
             int m = Integer.parseInt(st.nextToken());
-            int count = 0;
-
-            for (int j = n; j <= m; j++) {
-                char[] numChars = String.valueOf(j).toCharArray();
-                for (char numChar : numChars) {
-                    if (numChar == '0') {
-                        count++;
-                    }
-                }
-            }
-            System.out.println(count);
+            int cnt = 0;
+           for(int j= n;j<=m;j++){
+               char[] numChars = String.valueOf(j).toCharArray();
+               for(char numChar : numChars){
+                   if(numChar =='0'){
+                       cnt++;
+                   }
+               }
+           }
+            System.out.println(cnt);
         }
     }
 }
