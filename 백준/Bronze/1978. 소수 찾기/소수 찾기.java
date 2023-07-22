35d021e3f41b34414307c0e79ce1f902c06d5333
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-07-22 16:26:56 +0900

    [Bronze II] Title: 소수 찾기, Time: 76 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,46 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-
+class Main{
     static boolean[] primeNumber;
-
-    public static void main(String[] args) throws IOException {
+    
+    public static void main(String[]args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
+        
         etoche();
-
-        int count = 0;
-        for (int i = 0; i < N; i++) {
+        int cnt =0;
+        for(int i =0; i<N;i++){
             int num = Integer.parseInt(st.nextToken());
-            if (isPrime(num)) {
-                count++;
-            }
+            if(isPrime(num)) cnt++;
         }
-
-        System.out.println(count);
-
-        br.close();
+        System.out.print(cnt);
     }
-
-    private static void etoche() {
+    private static void etoche(){
         primeNumber = new boolean[1001];
         primeNumber[0] = primeNumber[1] = true;
-
-        for (int i = 2; i <= Math.sqrt(1000); i++) {
-            if (!primeNumber[i]) {
-                for (int j = i * i; j < primeNumber.length; j += i) {
-                    primeNumber[j] = true;
-                }
+        
+        for(int i=2;i<=Math.sqrt(1000);i++){
+            if(!primeNumber[i]){
+                for(int j=i*i ; j<primeNumber.length; j+=i)
+                    primeNumber[j]= true;
             }
         }
     }
-
-    private static boolean isPrime(int n) {
+    private static boolean isPrime(int n){
         return !primeNumber[n];
     }
 }
