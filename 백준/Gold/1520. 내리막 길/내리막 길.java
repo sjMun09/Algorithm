3ebf6d1a0a5b4f09ac3389d7d129166d3dd96a36
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-07-24 08:35:44 +0900

    [Gold III] Title: 내리막 길, Time: 324 ms, Memory: 39876 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,58 +1,60 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int M, N;
-    private static int[][] map;
-    private static int[][] dp;
-
-    private static int[] dx = {-1, 0, 1, 0};
-    private static int[] dy = {0, 1, 0, -1};
+    static int[][] adjacent;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+    static int n, m;
+    static int[][] memo;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-
-        map = new int[M][N];
-        dp = new int[M][N];
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                dp[i][j] = -1;
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        adjacent = new int[n][m];
+        memo = new int[n][m];
+        // memoization 초기화
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                memo[i][j] = -1;
             }
         }
 
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                adjacent[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
         System.out.println(dfs(0, 0));
     }
 
-    private static int dfs(int x, int y) {
-        if (x == M - 1 && y == N - 1) {
+    static int dfs(int x, int y) {
+        // base case
+        if (x == n - 1 && y == m - 1) {
             return 1;
         }
 
-        if (dp[x][y] != -1) {
-            return dp[x][y];
+        // 이미 방문하여 경우의 수를 세었다면 저장된 값을 사용
+        if (memo[x][y] != -1) {
+            return memo[x][y];
         }
 
-        dp[x][y] = 0;
+        memo[x][y] = 0;
         for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            int xx = x + dx[i];
+            int yy = y + dy[i];
 
-            if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
-                if (map[x][y] > map[nx][ny]) {
-                    dp[x][y] += dfs(nx, ny);
+            if (xx >= 0 && xx < n && yy >= 0 && yy < m) {
+                if (adjacent[xx][yy] < adjacent[x][y]) {
+                    memo[x][y] += dfs(xx, yy);
                 }
             }
         }
-
-        return dp[x][y];
+        return memo[x][y];
     }
 }
