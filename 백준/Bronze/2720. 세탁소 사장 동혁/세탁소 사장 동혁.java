5868a70ace62071ccc0eefaddaf1a2770a3b745d
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-07-25 16:12:11 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 252 ms, Memory: 15428 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -1,24 +1,20 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[]args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         
-        int[] coins = {25, 10, 5, 1};
-        
-        for (int i = 0; i < T; i++) {
+        int[] coins = {25,10,5,1};
+        for(int i=0; i<T;i++){
             int C = Integer.parseInt(br.readLine());
             int[] result = new int[4];
             
-            for (int j = 0; j < coins.length; j++) {
-                result[j] = C / coins[j];
+            for(int j=0;j<coins.length;j++){
+                result[j]= C/coins[j];
                 C %= coins[j];
             }
-            
-            System.out.printf("%d %d %d %d\n", result[0], result[1], result[2], result[3]);
+            System.out.printf("%d %d %d %d\n", result[0], result[1],result[2],result[3]);
         }
     }
 }
