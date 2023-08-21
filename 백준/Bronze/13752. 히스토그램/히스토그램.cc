bca13a1501916f1ac31b7eb74ac8e6cf28d6f50f
SeongJun <124541156+sjMun09@users.noreply.github.com>
2023-08-21 22:39:08 +0900

    [Bronze IV] Title: 히스토그램, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,a;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        for (int j = 0; j < a; j++)
+            cout << "=";
+        cout << '\n';
+    }
+}
