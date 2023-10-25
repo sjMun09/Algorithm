01a1160c70cd3316b91f88be3cdb5569bb251794
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2023-10-25 18:45:23 +0900

    [Bronze II] Title: 줄 세우기, Time: 120 ms, Memory: 12984 KB -BaekjoonHub

---
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.java
@@ -0,0 +1,18 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> students = new ArrayList<>();
+        int N = sc.nextInt(); // 학생 수
+
+        for (int i = 1; i <= N; i++) {
+            students.add(sc.nextInt(), i); // 학생 순서 입력 받고 넣기
+        }
+        for (int i = N - 1; i >= 0; i--) { // 뒤에서부터 출력
+            System.out.print(students.get(i) + " ");
+        }
+    }
+}
