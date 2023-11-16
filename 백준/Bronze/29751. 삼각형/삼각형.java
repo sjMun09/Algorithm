fd22b614594c2369999d134e2ab1d2575fa76ea2
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2023-11-16 09:15:40 +0900

    [Bronze V] Title: 삼각형, Time: 88 ms, Memory: 11768 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+
+        double area = (double)W * H / 2;
+
+        System.out.printf("%.1f\n", area);
+    }
+}
