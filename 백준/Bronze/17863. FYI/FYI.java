e3a59d16b1fa96aa979f53bc77bf6209c63f6a34
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2023-12-21 12:12:10 +0900

    [Bronze IV] Title: FYI, Time: 76 ms, Memory: 11468 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String number = br.readLine().trim();
+        System.out.println(number.startsWith("555") ? "YES" : "NO");
+    }
+}
