181dc7b60769f437b40673c057246eb23a4d55d4
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2024-01-17 07:52:08 +0900

    [Bronze IV] Title: 임스의 메이플컵, Time: 76 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/29790. 임스의 메이플컵/임스의 메이플컵.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int U = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        if(N >= 1000 && (U >= 8000 || L >= 260)) {
+            System.out.println("Very Good");
+        } else if(N >= 1000) {
+            System.out.println("Good");
+        } else {
+            System.out.println("Bad");
+        }
+    }
+}
