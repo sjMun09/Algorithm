1d21579d789d30bf6c5362db246aa7a2af126eb7
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2024-02-17 14:48:46 +0900

    [Bronze V] Title: Арифметическая магия, Time: 128 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/18096. Арифметическая магия/Арифметическая магия.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine(); // 입력받은 N을 사용하지 않으므로, 이를 읽기만 합니다.
+        System.out.println(1); // 결과는 항상 1입니다.
+    }
+}
