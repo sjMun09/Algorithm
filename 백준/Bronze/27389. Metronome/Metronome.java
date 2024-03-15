82793fe59b3f837e6de774e955a38b2f35a4ff96
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2024-03-16 08:44:45 +0900

    [Bronze V] Title: Metronome, Time: 136 ms, Memory: 14488 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        double rotations = n / 4.0;
+
+        System.out.printf("%.2f", rotations);
+    }
+}
