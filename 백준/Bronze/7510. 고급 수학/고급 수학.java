cce6707c6b13fb7f820637e715106231bb31c910
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2024-04-21 15:51:20 +0900

    [Bronze III] Title: 고급 수학, Time: 160 ms, Memory: 16020 KB -BaekjoonHub

---
+++ 백준/Bronze/7510. 고급 수학/고급 수학.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[3];
+		
+		for(int i = 1; i <= n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			arr[0] = Integer.parseInt(st.nextToken());
+			arr[1] = Integer.parseInt(st.nextToken());
+			arr[2] = Integer.parseInt(st.nextToken());
+			
+			//조건과 상관없이 항상 출력되는 출력문
+			System.out.println("Scenario #" + i + ":");
+			
+			//정렬
+			Arrays.sort(arr);
+			
+			if((arr[0] * arr[0]) + (arr[1] * arr[1]) == (arr[2] * arr[2])) {
+				System.out.println("yes");
+			}else {
+				System.out.println("no");
+			}
+			//케이스 마다 사이에 빈줄을 출력
+			System.out.println();
+		}
+	}
+
+}
