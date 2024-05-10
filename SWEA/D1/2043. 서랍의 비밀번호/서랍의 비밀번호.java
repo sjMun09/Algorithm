bafbbaf6ec7302e8e745e115c2a8418dabe2a3b3
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2024-05-10 12:42:00 +0900

    [D1] Title: 서랍의 비밀번호, Time: 101 ms, Memory: 22,108 KB -BaekjoonHub

--- SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
@@ -1,18 +1,23 @@
-import java.util.*;
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 /*
 pw : 009, k : 980일 경우 해결한 원형 계산 적용.
 */
-class Solution {
-    public static void main(String args[]) throws Exception {
+public class Solution {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String[] input = br.readLine().split(" ");
+        int pw = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
 
-        int pw = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int attempts;
+        if (pw >= k) {
+            attempts = pw - k + 1;
+        } else {
+            attempts = 1000 - k + pw + 1;
+        }
 
-        System.out.print(pw-k+1);
-
+        System.out.print(attempts);
     }
 }
