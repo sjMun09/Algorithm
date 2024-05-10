5e0c85b2600d8c113fa4de5501e5b60f41cf1d55
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2024-05-10 11:24:12 +0900

    [D1] Title: 1대1 가위바위보, Time: 113 ms, Memory: 23,324 KB -BaekjoonHub

--- SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.java
@@ -7,9 +7,9 @@ class Solution{
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-
-        if (a == 1 && b == 3 || a == 2 && b == 1 || a == 3 && b == 2) {
+        if ((a % 3) + 1 == b) {
+            System.out.print("B");
+        } else
             System.out.print("A");
-        }else System.out.print("B");
-	}
+    }
 }
