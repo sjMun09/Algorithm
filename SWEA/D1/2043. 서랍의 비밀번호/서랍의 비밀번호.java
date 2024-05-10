b84d896c802c5c375eb3beb3a4b734e554186de0
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2024-05-10 12:40:43 +0900

    [D1] Title: 서랍의 비밀번호, Time: 100 ms, Memory: 21,296 KB -BaekjoonHub

--- SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
@@ -1,6 +1,9 @@
 import java.util.*;
 import java.io.*;
 
+/*
+pw : 009, k : 980일 경우 해결한 원형 계산 적용.
+*/
 class Solution {
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
