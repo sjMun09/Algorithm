957721cb189221ec3f1db2e024c978318d6193f4
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2024-10-06 22:05:13 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 15972 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,58 +1,68 @@
-// 문제에 대해 좀더 이해하기 쉬운 링크 https://lotuus.tistory.com/98
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    static int T, M, N, K, count;
+
+class Main {
+    /**
+     * 좌표
+     */
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
     static int[][] field;
-    static boolean[][] visited;
+    static boolean[][] visit;
+    static int M,N,K;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        T = Integer.parseInt(br.readLine());
-
+        /**
+         * 1012번, dfs 배추심기.
+         */
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         for (int t = 0; t < T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            M = Integer.parseInt(st.nextToken());
-            N = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            M = Integer.parseInt(st.nextToken()); // width
+            N = Integer.parseInt(st.nextToken()); // height
+            K = Integer.parseInt(st.nextToken()); //count
 
             field = new int[N][M];
-            visited = new boolean[N][M];
+            visit = new boolean[N][M];
 
-            for (int k = 0; k < K; k++) {
+            //input
+            for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
-                int y = Integer.parseInt(st.nextToken());
                 int x = Integer.parseInt(st.nextToken());
-                field[x][y] = 1;
+                int y = Integer.parseInt(st.nextToken());
+                field[y][x] =1; // 1은 배추가 심어져 있는 땅을 나타낸다.
             }
 
-            count = 0;
+            // cnt++;
+            int cnt = 0;
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < M; j++) {
-                    if (dfs(i, j)) {
-                        count++;
+                    if (field[i][j] == 1 && !visit[i][j]) {
+                        dfs(i,j);
+                        cnt++;
                     }
                 }
             }
-            System.out.println(count);
+            sb.append(cnt).append("\n"); // record
         }
+        System.out.print(sb.toString());
     }
 
-    public static boolean dfs(int x, int y) {
-        if (x < 0 || x >= N || y < 0 || y >= M || visited[x][y] || field[x][y] == 0) {
-            return false;
+    /**
+     * dfs
+     */
+    static void dfs(int y, int x) {
+        visit[y][x] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int ny = y + dy[i]; // y
+            int nx = x + dx[i]; // x
+
+            // checking // RECURSIVE
+            if(ny>=0 && ny<N && nx>=0 && nx<M && field[ny][nx]==1 && !visit[ny][nx]) dfs(ny, nx);
         }
-
-        visited[x][y] = true;
-
-        dfs(x - 1, y);
-        dfs(x + 1, y);
-        dfs(x, y - 1);
-        dfs(x, y + 1);
-
-        return true;
     }
 }
