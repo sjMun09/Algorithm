86d609794c7f8bf44fd1441784a5e268220a8d74
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2024-12-11 16:27:19 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 112 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,22 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
-class Main{
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int sum = Integer.MAX_VALUE;
-        StringTokenizer sub = new StringTokenizer(br.readLine(), "-");
-        while (sub.hasMoreTokens()) {
-            int tmp =0;
-            StringTokenizer add = new StringTokenizer(sub.nextToken(), "+");
-            while (add.hasMoreTokens()) {
-                tmp += Integer.parseInt(add.nextToken());
-            }
-            if (sum == Integer.MAX_VALUE)sum = tmp;
-            else sum -= tmp;
+
+        String expression = br.readLine();
+
+        String[] splitByMinus = expression.split("-");
+
+        int result = sumOfSubExpression(splitByMinus[0]);
+
+        for (int i = 1; i < splitByMinus.length; i++) {
+            result -= sumOfSubExpression(splitByMinus[i]);
         }
-        System.out.print(sum);
+
+        System.out.print(result);
+    }
+
+    private static int sumOfSubExpression(String subExpression) {
+        int sum = 0;
+        String[] splitByPlus = subExpression.split("\\+"); 
+        for (String num : splitByPlus) {
+            sum += Integer.parseInt(num); 
+        }
+        return sum;
     }
 }
