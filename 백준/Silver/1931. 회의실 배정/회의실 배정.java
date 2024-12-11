9bf64e638eb200e9516e676a38329d07387305ea
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2024-12-11 16:18:05 +0900

    [Silver I] Title: 회의실 배정, Time: 520 ms, Memory: 45728 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -1,62 +1,50 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-class meetingRoom implements Comparable<meetingRoom> {
-    private int startTime;
-    private int endTime;
-
-    public meetingRoom(int startTime, int endTime) {
-        this.startTime = startTime;
-        this.endTime = endTime;
-    }
-
-    @Override
-    public int compareTo(meetingRoom meetingRoom) {
-        if (this.endTime == meetingRoom.endTime) {
-            return this.startTime - meetingRoom.startTime;
-        }
-        return this.endTime - meetingRoom.endTime;
-    }
-
-    public int getStartTime() {
-        return startTime;
-    }
-
-    public int getEndTime() {
-        return endTime;
-    }
-}
-
 public class Main {
+    static class Meeting implements Comparable<Meeting> {
+        int start;
+        int end;
+
+        public Meeting(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public int compareTo(Meeting other) {
+            if (this.end == other.end) {
+                return this.start - other.start;
+            }
+            return this.end - other.end;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
-        meetingRoom[] meetingRooms = new meetingRoom[n];
+        int n = Integer.parseInt(br.readLine()); 
+        Meeting[] meetings = new Meeting[n];
 
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int startTime = Integer.parseInt(st.nextToken());
-            int endTime = Integer.parseInt(st.nextToken());
-
-            meetingRooms[i] = new meetingRoom(startTime, endTime);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            meetings[i] = new Meeting(start, end);
         }
 
-        Arrays.sort(meetingRooms);
-
+        Arrays.sort(meetings);
         int count = 0;
-        int end = 0;
+        int lastEndTime = 0;
 
         for (int i = 0; i < n; i++) {
-            if (end <= meetingRooms[i].getStartTime()) {
-                end = meetingRooms[i].getEndTime();
+            if (meetings[i].start >= lastEndTime) {
                 count++;
+                lastEndTime = meetings[i].end;
             }
         }
-
         System.out.print(count);
     }
 }
