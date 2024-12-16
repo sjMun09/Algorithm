26498051bab1cebcfd4e6b6f441419f7dd64fb45
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2024-12-16 16:28:04 +0900

    [Silver III] Title: 01타일, Time: 124 ms, Memory: 18648 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        int[] dp = new int[N + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+        }
+
+        System.out.print(dp[N]);
+    }
+}
