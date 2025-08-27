e55c69f18c79a700a755276551d5717846c31dce
Chorong0824 <124541156+sjMun09@users.noreply.github.com>
2025-08-27 10:03:44 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 320 ms, Memory: 20436 KB -BaekjoonHub

--- 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.java
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.java
@@ -1,32 +1,19 @@
-import java.io.*;
-import java.math.BigInteger;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-//           홀수행일 경우 홀수 출력들은 별, 그리고 짝수 출력들은 공백
-            if (i % 2 == 1) {
-                for (int j = 0; j < 2 * n; j++) {
-                    if (j % 2 == 1) {
-                        System.out.print("*");
-                    } else {
-                        System.out.print(" ");
-                    }
-                }
-            } else {  // 짝수 행에서도 마찬가지로 N개의 출력을 하게 되는데 홀수 출력들은 공백, 그리고 짝수 출력들은 별을 출력
-                for (int j = 0; j < 2 * n; j++) {
-                    if (j % 2 == 1) {
-                        System.out.print(" ");
-                    } else {
-                        System.out.print("*");
-                    }
-                }
-            }
-            System.out.println();
-        }
-    }
+		for (int i = 1; i <= n; i++) {
+			if (i % 2 == 0)  System.out.print(" ");
+				for (int j = 1; j <= n; j++) {
+					System.out.print("*");
+					if (j < n) System.out.print(" ");
+				}
+				if (i < n) System.out.println();
+		}
+	}
 }
